92b9f71f274fde250a73d42c2a5b9c0cf8b2447e
SnailData <134276469+SnailData@users.noreply.github.com>
2023-05-22 13:09:40 +0300

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My Partents New Helthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
